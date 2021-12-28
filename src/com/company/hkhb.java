f83800f1cdc237483f3b652c97e2f43413743585
kapyt <kapytokboris@mail.ru>
2021-12-28 11:45:41 +0300

    adasdasda

--- src/com/company/hkhb.java
+++ src/com/company/hkhb.java
@@ -4,5 +4,7 @@ public class hkhb {
     public void out() {
         System.out.println("test2");
         System.out.println("Hello");
+
+        System.out.println("Some changes in master");
     }
 }
